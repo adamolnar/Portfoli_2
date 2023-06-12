38354238668854ca0a2317d485c13aa840f6dbd0
adamolnar <adamolnar121@gmail.com>
2023-06-12 18:59:06 +0000

    Add confetti animation to win.html from CodePen

--- assets/js/script.js
+++ assets/js/script.js
@@ -109,8 +109,9 @@ function handlePlayerChoice(event) {
       if (moves == 10) {
             gameOver();
       } else {
-            shuffleIntervalID;
+            shuffleEmojis;
             nextRoundBtn;
+            document.getElementsByClassName('emoji').innerHTML;
       }
 }
 
@@ -149,11 +150,11 @@ function determineWinner() {
         computerScore +=0;
         computerScoreBoard.textContent = computerScore;
        
-    } else if (userChoice === "🪨" && computerChoice === "✂️") {
+    } else if (userChoice === "🪨" && computerChoice === "✂️ ") {
         gameResultMessage = 'Rock crushes scissors. You win !';
         playerScore +=1;
         playerScoreBoard.textContent = playerScore;
-    } else if (userChoice === "✂️" && computerChoice === "🪨") {
+    } else if (userChoice === "✂️ " && computerChoice === "🪨") {
         gameResultMessage = 'Rock crushes scissors ! You lose';
         computerScore +=1;
         computerScoreBoard.textContent = computerScore;
@@ -167,11 +168,11 @@ function determineWinner() {
         computerScore +=1;
         computerScoreBoard.textContent = computerScore;
 
-    } else if (userChoice === "✂️" && computerChoice === "📄") {
+    } else if (userChoice === "✂️ " && computerChoice === "📄") {
         gameResultMessage =  'Scissors cut paper. You win !';
         playerScore +=1;
         playerScoreBoard.textContent = playerScore;
-    } else if (userChoice === "📄" && computerChoice === "✂️") {
+    } else if (userChoice === "📄" && computerChoice === "✂️ ") {
         gameResultMessage =  'Scissors cut paper ! You lose';
         computerScore +=1;
         computerScoreBoard.textContent = computerScore;
@@ -221,11 +222,11 @@ function determineWinner() {
           computerScore +=1;  
           computerScoreBoard.textContent = computerScore;
 
-    }else if (userChoice === "🖖🏻" && computerChoice === "✂️") {
+    }else if (userChoice === "🖖🏻" && computerChoice === "✂️ ") {
           gameResultMessage = 'Spock crushes scissors. You win !';
           playerScore +=1;
           playerScoreBoard.textContent = playerScore;
-    }else if (userChoice === "✂️" && computerChoice === "🖖🏻") {
+    }else if (userChoice === "✂️ " && computerChoice === "🖖🏻") {
           gameResultMessage = 'Spock crushes scissors ! You lose'; 
           computerScore +=1; 
           computerScoreBoard.textContent = computerScore;
@@ -257,140 +258,12 @@ function gameOver() {
 
 
 //------------------------GAME OVER---------------------------------
-// Function to dispaly Game Over 
-function myoutput() {
-      document.getElementById('_gover').innerHTML = "Game Over";
-      setTimeout(function () {
-        document.getElementById('_gover').innerHTML = "";
-      }, 500);
-    }
-  this.setInterval(myoutput,1000);
-
-
 // Function to play again game and redirect it to game.html
 function playAgain () {
       window.location.href = "game.html"; 
 };      
 
 
-//----------------------- CONFETTI FROM CODEHIM--------------------------
-//Var Inits
-canvas = document.getElementById("canvas");
-ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-cx = ctx.canvas.width / 2;
-cy = ctx.canvas.height / 2;
-
-let confetti = [];
-const confettiCount = 300;
-const gravity = 0.5;
-const terminalVelocity = 5;
-const drag = 0.075;
-const colors = [
-{ front: 'red', back: 'darkred' },
-{ front: 'green', back: 'darkgreen' },
-{ front: 'blue', back: 'darkblue' },
-{ front: 'yellow', back: 'darkyellow' },
-{ front: 'orange', back: 'darkorange' },
-{ front: 'pink', back: 'darkpink' },
-{ front: 'purple', back: 'darkpurple' },
-{ front: 'turquoise', back: 'darkturquoise' }];
-
-
-//-----------Functions--------------
-resizeCanvas = () => {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  cx = ctx.canvas.width / 2;
-  cy = ctx.canvas.height / 2;
-};
-
-randomRange = (min, max) => Math.random() * (max - min) + min;
-
-initConfetti = () => {
-  for (let i = 0; i < confettiCount; i++) {
-    confetti.push({
-      color: colors[Math.floor(randomRange(0, colors.length))],
-      dimensions: {
-        x: randomRange(10, 20),
-        y: randomRange(10, 30) },
-
-      position: {
-        x: randomRange(0, canvas.width),
-        y: canvas.height - 1 },
-
-      rotation: randomRange(0, 2 * Math.PI),
-      scale: {
-        x: 1,
-        y: 1 },
-
-      velocity: {
-        x: randomRange(-25, 25),
-        y: randomRange(0, -50) } });
-
-
-  }
-};
-
-//---------Render-----------
-render = () => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  confetti.forEach((confetto, index) => {
-    let width = confetto.dimensions.x * confetto.scale.x;
-    let height = confetto.dimensions.y * confetto.scale.y;
-
-    // Move canvas to position and rotate
-    ctx.translate(confetto.position.x, confetto.position.y);
-    ctx.rotate(confetto.rotation);
-
-    // Apply forces to velocity
-    confetto.velocity.x -= confetto.velocity.x * drag;
-    confetto.velocity.y = Math.min(confetto.velocity.y + gravity, terminalVelocity);
-    confetto.velocity.x += Math.random() > 0.5 ? Math.random() : -Math.random();
-
-    // Set position
-    confetto.position.x += confetto.velocity.x;
-    confetto.position.y += confetto.velocity.y;
-
-    // Delete confetti when out of frame
-    if (confetto.position.y >= canvas.height) confetti.splice(index, 1);
-
-    // Loop confetto x position
-    if (confetto.position.x > canvas.width) confetto.position.x = 0;
-    if (confetto.position.x < 0) confetto.position.x = canvas.width;
-
-    // Spin confetto by scaling y
-    confetto.scale.y = Math.cos(confetto.position.y * 0.1);
-    ctx.fillStyle = confetto.scale.y > 0 ? confetto.color.front : confetto.color.back;
-
-    // Draw confetti
-    ctx.fillRect(-width / 2, -height / 2, width, height);
-
-    // Reset transform matrix
-    ctx.setTransform(1, 0, 0, 1, 0, 0);
-  });
-
-  // Fire off another round of confetti
-  if (confetti.length <= 10) initConfetti();
-
-  window.requestAnimationFrame(render);
-};
-
-//---------Execution--------
-initConfetti();
-render();
-
-//----------Resize----------
-window.addEventListener('resize', function () {
-  resizeCanvas();
-});
-
-//------------Click------------
-window.addEventListener('click', function () {
-  initConfetti();
-});
 
 
 
